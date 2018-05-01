8315442b109fd487137e345a5b4a913637bd501b
semitro <semitro_8@mail.ru>
2018-05-01 16:44:35 +0300

    reading word..

--- src/main.asm
+++ src/main.asm
@@ -2,6 +2,8 @@ global _start
 
 extern parse_int
 extern read_word
+extern read_char
+extern print_string
 
 %include "io_lib.inc"
 %include "fmachine.inc"
@@ -11,6 +13,8 @@ _start:
 .interp_loop:
 .read_word:
 	mov rdi, input_buffer 
+	mov rsi, INPUT_BUFFER_SIZE
 	call read_word
+
 .isNum:
 	mov rdi, input_buffer	
